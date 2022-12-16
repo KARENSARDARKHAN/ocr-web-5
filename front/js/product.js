3dce1b5c50272899cdac81c5706556c76c68629f
KARENSARDARKHAN <108447157+KARENSARDARKHAN@users.noreply.github.com>
2022-12-16 04:59:32 +0100

    Price Description Title Img src  Product page

--- front/js/product.js
+++ front/js/product.js
@@ -11,6 +11,30 @@ async function init() {
 
   let spanPrice = document.getElementById("price");
   spanPrice.innerHTML = product.price;
+
+  let spanDescription = document.getElementById("description");
+  spanDescription.innerHTML = product.description;
+
+  let spanName = document.getElementById("title");
+  spanName.innerHTML = product.name;
+
+  document.querySelector(
+    ".item__img"
+  ).innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
 }
 
 init();
+
+//let htmlProduct = `<a href="./product.html?id=${product._id}">
+//<article>
+// <img src="${product.imageUrl}" alt='${product.altTxt}'>
+//<h3 class="productName">${product.name}</h3>
+
+//document
+// .getElementsByClassName("item__img")
+//.setAttribute("src", "${product.imageUrl}");
+
+//document
+//.getElementsByClassName("item__img")
+//.setAttribute("src", "${product.imageUrl}");
+//spanImageUrl.innerHTML = product.imageUrl;
