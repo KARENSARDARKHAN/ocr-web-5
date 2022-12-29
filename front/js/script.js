d5284691306b21caeaac9ebb2da273171764937f
KARENSARDARKHAN <108447157+KARENSARDARKHAN@users.noreply.github.com>
2022-12-29 05:34:44 +0100

    ITEMS IMPORTED FROM API TO APPEAR ON INDEX PAGE

--- front/js/script.js
+++ front/js/script.js
@@ -1,1 +1,27 @@
+// Récupération du code HTML et Répartition des données de l'API dans le DOM
+function htmlItemCard(product) {
+  return `<a href="./product.html?id=${product._id}">
+  <article>
+    <img src="${product.imageUrl}" alt='${product.altTxt}'>
+    <h3 class="productName">${product.name}</h3>
+    <p class="productDescription">${product.description}</p>
+  </article>
+  </a>`;
+}
+
+// Récupération des articles de l'API
+let response = await fetch("http://localhost:3000/api/products");
+let products = await response.json();
+
+//Utilisation de la propriété .innerHTML pour vider le HTML de la section
+let sectionItems = document.getElementById("items");
+sectionItems.innerHTML = "";
+
+//Boucle for modifiant le HTML et générant l'apparition des cartes produits
+//les unes après les autres dans la page web.
+for (let index = 0; index < products.length; index++) {
+  let productData = products[index];
+  let productCardHtml = htmlItemCard(productData);
+  sectionItems.innerHTML = sectionItems.innerHTML + productCardHtml;
+}
 
